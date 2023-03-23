4ecba18a1a472b1a3a9296b7f938e64d9668c526
Samy NALBANDIAN <samy.nalbandian-prestataire@bforbank.com>
2023-03-23 12:07:29 +0100

    refactor: oneline axios client config

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { environment } from "./environment";
 
 export const apiClient = axios.create({
-    withCredentials: true,
+    baseURL: environment.host,
+    headers: {
+        'x-auth-token': environment.playerToken
+    }
 });
-
-apiClient.defaults.headers.common['x-auth-token'] = environment.playerToken;
-apiClient.defaults.baseURL = environment.host;
