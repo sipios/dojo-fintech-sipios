d06ea41fcd1a1658fc3ab76aefed1af128727449
Youssef Benkirane <youssefb@sipios.com>
2023-04-04 10:36:33 +0200

    (feat/modal-payment) : Create PaymentModal component

---
+++ src/components/PaymentModal.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { postPayment, PostPaymentBody } from "../utils/post-payment";
+
+interface PaymentModalProp {
+  skinId: number;
+}
+
+export function PaymentModal({ skinId }: PaymentModalProp) {
+  return (
+    <>
+    </>
+  );
+}
