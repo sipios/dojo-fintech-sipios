2cf8e3479e4aaa9764f9408e54f0c8ee4639359e
Youssef Benkirane <youssefb@sipios.com>
2023-04-04 10:37:28 +0200

    (feat/modal-payment) : Add styled form

--- src/components/PaymentModal.tsx
+++ src/components/PaymentModal.tsx
@@ -8,6 +8,7 @@ interface PaymentModalProp {
 export function PaymentModal({ skinId }: PaymentModalProp) {
   return (
     <>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"></form>
     </>
   );
 }
