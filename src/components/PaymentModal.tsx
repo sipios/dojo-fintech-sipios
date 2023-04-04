272084b3105201e1804c5d60ec4be65d12f332ed
Youssef Benkirane <youssefb@sipios.com>
2023-04-04 10:38:42 +0200

    (feat/modal-payment) : Add Card Serial Input in the form

--- src/components/PaymentModal.tsx
+++ src/components/PaymentModal.tsx
@@ -6,9 +6,29 @@ interface PaymentModalProp {
 }
 
 export function PaymentModal({ skinId }: PaymentModalProp) {
+  const [cardSerial, setCardSerial] = useState<string>("");
+  
   return (
     <>
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"></form>
+
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="card_serial"
+        >
+          Card Serial
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="card_serial"
+            type="text"
+            placeholder="Card Serial"
+            onChange={(inputCardSerial) => {
+              setCardSerial(inputCardSerial.target.value);
+            }}
+          />
+        </label>
+      </div>
     </>
   );
 }
