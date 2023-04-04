bd6f8bf36fecb7079834558a6a80ff433948796e
PEDENAUD <LAURA>
2023-04-04 12:12:02 +0200

    (feat/modal-payment): Add modal tailwind in PaymentModal.tsx

--- src/components/PaymentModal.tsx
+++ src/components/PaymentModal.tsx
@@ -20,40 +20,41 @@ export function PaymentModal({ skinId, setShowModal }: PaymentModalProp) {
 
   return (
     <>
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"></form>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="block text-gray-700 text-sm font-bold mb-2">
+            Skin #{skinId}
+          </h1>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="card_serial"
+              type="text"
+              placeholder="Card Serial"
+              onChange={(inputCardSerial) => {
+                setCardSerial(inputCardSerial.target.value);
+              }}
+            />
+          </div>
 
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="card_serial"
-        >
-          Card Serial
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="card_serial"
-            type="text"
-            placeholder="Card Serial"
-            onChange={(inputCardSerial) => {
-              setCardSerial(inputCardSerial.target.value);
-            }}
-          />
-        </label>
-      </div>
-
-      <div className="flex items-center justify-between">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={(event) => {
-            event.preventDefault();
-            const postPaymentBody: PostPaymentBody = {
-              card_serial: cardSerial,
-              skin_id: skinId,
-            };
-            postPayment(postPaymentBody);
-          }}
-        >
-          Submit
-        </button>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+              onClick={submitPayment}
+            >
+              Submit
+            </button>
+            <div className="flex items-center justify-end p-6 rounded-b">
+              <button
+                className="text-red-500 background-transparent font-bold uppercase text-sm ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
